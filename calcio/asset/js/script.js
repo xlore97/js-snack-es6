7ba471045e81e9f6121e3bd6eba49f37f86d2190
Xlore97 <lorenzo.manetti.photo@gmail.com>
2025-09-09 18:14:28 +0200

    Fine secondo snack

--- calcio/asset/js/script.js
+++ calcio/asset/js/script.js
@@ -25,3 +25,23 @@ const squadre = [
         falli: 0,
     },
 ]
+
+
+function generaRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (let i = 0; i < squadre.length; i++) {
+    squadre[i].punti = generaRandomNum(0, 90);
+    squadre[i].falli = generaRandomNum(0, 100);
+}
+
+const risultatiFalli = squadre.map(squadra => {
+    return {
+        nome: squadra.nome,
+        falli: squadra.falli
+    };
+});
+
+console.log('Squadre con punti e falli generati:', squadre);
+console.log('Solo nomi e falli subiti:', risultatiFalli);
