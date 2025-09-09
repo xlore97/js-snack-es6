768131876e2134e9afca6fe2536e1abd562fe7ac
Xlore97 <lorenzo.manetti.photo@gmail.com>
2025-09-09 17:57:32 +0200

    Calcio creazione array con oggetti

---
+++ calcio/asset/js/script.js
@@ -0,0 +1,27 @@
+const squadre = [
+    {
+        nome: 'Fiorentina',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Roma',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Inter',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Milan',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Napoli',
+        punti: 0,
+        falli: 0,
+    },
+]
